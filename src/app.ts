652a6c89fc658cff7b2c6b5cdb29d2bac4bd8d4c
Ihor Kytsak <kitsak735@gmail.com>
2022-08-04 21:12:03 +0300

    dom selection && rendering

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,26 @@
-// Code goes here!
-console.log('hi');
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedMode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedMode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prhInput = new ProjectInput();
